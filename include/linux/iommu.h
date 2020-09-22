ddb07d3ebdce725abe9bf775e2384b9be88b8d09
Joerg Roedel <jroedel@suse.de>
2020-09-22 16:39:04 +0800

    iommu: Introduce wrappers around dev->iommu_fwspec
    mainline inclusion
    from mainline-v4.20-rc1
    commit b4ef725eeba158f365da9de1f05149094643ddea
    category:feature
    bugzilla:NA
    CVE:NA
    
    -------------------
    
    These wrappers will be used to easily change the location of
    the field later when all users are converted.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
     Conflicts:
    	include/linux/iommu.h
    
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -671,6 +671,17 @@ void iommu_fwspec_free(struct device *dev);
 int iommu_fwspec_add_ids(struct device *dev, u32 *ids, int num_ids);
 const struct iommu_ops *iommu_ops_from_fwnode(struct fwnode_handle *fwnode);
 
+static inline struct iommu_fwspec *dev_iommu_fwspec_get(struct device *dev)
+{
+	return dev->iommu_fwspec;
+}
+
+static inline void dev_iommu_fwspec_set(struct device *dev,
+					struct iommu_fwspec *fwspec)
+{
+	dev->iommu_fwspec = fwspec;
+}
+
 extern int iommu_sva_bind_device(struct device *dev, struct mm_struct *mm,
 				int *pasid, unsigned long flags, void *drvdata);
 extern int iommu_sva_unbind_device(struct device *dev, int pasid);
