86f6eb7c0be8349a131bd755644df0022295759c
Chris Down <chris@chrisdown.name>
2020-10-10 15:40:14 +0800

    mm: memcontrol: expose THP events on a per-memcg basis
    mainline inclusion
    from mainline-5.1-rc1
    commit 1ff9e6e1798c7670ea6a7680a1ad5582df2fa914
    category: bugfix
    bugzilla: 34611
    CVE: NA
    
    -------------------------------------------------
    
    Currently THP allocation events data is fairly opaque, since you can
    only get it system-wide.  This patch makes it easier to reason about
    transparent hugepage behaviour on a per-memcg basis.
    
    For anonymous THP-backed pages, we already have MEMCG_RSS_HUGE in v1,
    which is used for v1's rss_huge [sic].  This is reused here as it's
    fairly involved to untangle NR_ANON_THPS right now to make it per-memcg,
    since right now some of this is delegated to rmap before we have any
    memcg actually assigned to the page.  It's a good idea to rework that,
    but let's leave untangling THP allocation for a future patch.
    
    [akpm@linux-foundation.org: fix build]
    [chris@chrisdown.name: fix memcontrol build when THP is disabled]
      Link: http://lkml.kernel.org/r/20190131160802.GA5777@chrisdown.name
    Link: http://lkml.kernel.org/r/20190129205852.GA7310@chrisdown.name
    Signed-off-by: Chris Down <chris@chrisdown.name>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Roman Gushchin <guro@fb.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 1ff9e6e1798c7670ea6a7680a1ad5582df2fa914)
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    
     Conflicts:
    	mm/memcontrol.c
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1076,6 +1076,7 @@ static void collapse_huge_page(struct mm_struct *mm,
 	BUG_ON(!pmd_none(*pmd));
 	page_add_new_anon_rmap(new_page, vma, address, true);
 	mem_cgroup_commit_charge(new_page, memcg, false, true);
+	count_memcg_events(memcg, THP_COLLAPSE_ALLOC, 1);
 	lru_cache_add_active_or_unevictable(new_page, vma);
 	pgtable_trans_huge_deposit(mm, pmd, pgtable);
 	set_pmd_at(mm, address, pmd, _pmd);
@@ -1531,6 +1532,7 @@ tree_unlocked:
 		page_ref_add(new_page, HPAGE_PMD_NR - 1);
 		set_page_dirty(new_page);
 		mem_cgroup_commit_charge(new_page, memcg, false, true);
+		count_memcg_events(memcg, THP_COLLAPSE_ALLOC, 1);
 		lru_cache_add_anon(new_page);
 
 		/*
