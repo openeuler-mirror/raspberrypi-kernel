6e1b18763d100cc07b27b45ba12c5dfc53deeb8d
Ding Tianhong <dingtianhong@huawei.com>
2020-11-23 11:09:55 +0800

    ascend: share_pool: support debug mode and refactor some functions
    ascend inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    -------------------------------------------------
    
    The share pool is used widely for several accelerator, and it is
    difficult to debug the user problem, so add debug mode to analyse
    the problem, this mode is enabled by the sysctl_sp_debug_mode flag.
    
    Some functions have been refactored to protect the critical area
    correctly, and output message more clearly.
    
    Signed-off-by: Tang Yizhou <tangyizhou@huawei.com>
    Signed-off-by: Zhou Guanghui <zhouguanghui1@huawei.com>
    Signed-off-by: Wu Peng <wupeng58@huawei.com>
    Signed-off-by: Ding Tianhong <dingtianhong@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1737,6 +1737,15 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+	{
+		.procname	= "sharepool_debug_mode",
+		.data		= &sysctl_sp_debug_mode,
+		.maxlen		= sizeof(sysctl_sp_debug_mode),
+		.mode		= 0600,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
 #endif
 	{ }
 };
