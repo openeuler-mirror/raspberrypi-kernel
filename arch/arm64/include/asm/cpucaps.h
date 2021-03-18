9d58a2d3b9709751db681a37d4d31f4d3ad0aecb
Weilong Chen <chenweilong@huawei.com>
2021-03-18 14:47:07 +0800

    cache: Workaround HiSilicon Taishan DC CVAU
    ascend inclusion
    category: feature
    bugzilla: 46922
    CVE: NA
    
    -------------------------------------
    
    Taishan's L1/L2 cache is inclusive, and the data is consistent.
    Any change of L1 does not require DC operation to brush CL in L1 to L2.
    It's safe that don't clean data cache by address to point of unification.
    
    Without IDC featrue, kernel needs to flush icache as well as dcache,
    causes performance degradation.
    
    The flaw refers to V110/V200 variant 1.
    
    Signed-off-by: Weilong Chen <chenweilong@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Ding Tianhong <dingtianhong@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -71,7 +71,8 @@
 #define ARM64_HAS_TLB_RANGE			50
 #define ARM64_HAS_RNG				51
 #define ARM64_HAS_E0PD				52
+#define ARM64_WORKAROUND_HISILICON_1980005	53
 
-#define ARM64_NCAPS				53
+#define ARM64_NCAPS				54
 
 #endif /* __ASM_CPUCAPS_H */
