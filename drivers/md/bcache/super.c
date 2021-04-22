3ccf6ef0eba395bb3f0b63081909652cc4d344de
Li Ruilin <liruilin4@huawei.com>
2021-04-22 20:05:06 +0800

    bcache: add a framework to perform prefetch
    euleros inclusion
    category: feature
    bugzilla: https://bugzilla.openeuler.org/show_bug.cgi?id=26
    CVE: NA
    
    ------------------------------
    
    Add a framwork to transform io informations to userspace client and
    process prefetch request sent by userspace client. Create a char device
    namede "acache" for connecting between kernelspace and userspace.
    Save informations of all io requests into a buffer and pass them to
    client when client reads from the device.
    
    The prefetch request could be treated as normal io request. As deference,
    those requests have no need return data back to userspace, and they
    should not append readahead part.
    
    Add two parameters. acache_dev_size is for controlling size of buffer
    to save io informations. acache_prefetch_workers is for controlling
    max threads to process prefetch requests.
    
    Signed-off-by: Li Ruilin <liruilin4@huawei.com>
    Reviewed-by: Luan Jianhai <luanjianhai@huawei.com>
    Reviewed-by: Peng Junyi <pengjunyi1@huawei.com>
    Acked-by: Xie Xiuqi <xiexiuqi@huawei.com>
    Signed-off-by: Cheng Jian <cj.chengjian@huawei.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -8,6 +8,7 @@
  */
 
 #include "bcache.h"
+#include "acache.h"
 #include "btree.h"
 #include "debug.h"
 #include "extents.h"
@@ -2625,6 +2626,7 @@ static void bcache_exit(void)
 
 	if (bcache_major)
 		unregister_blkdev(bcache_major, "bcache");
+	acache_dev_exit();
 	unregister_reboot_notifier(&reboot);
 	mutex_destroy(&bch_register_lock);
 }
@@ -2695,6 +2697,8 @@ static int __init bcache_init(void)
 
 	bch_debug_init();
 	closure_debug_init();
+	if (acache_dev_init())
+		goto err;
 
 	bcache_is_reboot = false;
 
