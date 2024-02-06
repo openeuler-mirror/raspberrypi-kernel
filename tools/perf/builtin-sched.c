6b5c96ed245524eec53a3c0c6efd744246438648
Yang Jihong <yangjihong1@huawei.com>
2024-02-06 08:32:26 +0000

    perf sched: Move curr_thread initialization to perf_sched__map()
    stable inclusion
    from stable-v6.6.57
    commit 4078f1d442d8bdffba3fc4d4207849aee718a691
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB2M97
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=4078f1d442d8bdffba3fc4d4207849aee718a691
    
    --------------------------------
    
    [ Upstream commit 5e895278697c014e95ae7ae5e79a72ef68c5184e ]
    
    The curr_thread is used only for the 'perf sched map'. Put initialization
    in perf_sched__map() to reduce unnecessary actions in other commands.
    
    Simple functional testing:
    
      # perf sched record perf bench sched messaging
      # Running 'sched/messaging' benchmark:
      # 20 sender and receiver processes per group
      # 10 groups == 400 processes run
    
           Total time: 0.197 [sec]
      [ perf record: Woken up 1 times to write data ]
      [ perf record: Captured and wrote 15.526 MB perf.data (140095 samples) ]
    
      # perf sched map
        *A0                                                               451264.532445 secs A0 => migration/0:15
        *.                                                                451264.532468 secs .  => swapper:0
         .  *B0                                                           451264.532537 secs B0 => migration/1:21
         .  *.                                                            451264.532560 secs
         .   .  *C0                                                       451264.532644 secs C0 => migration/2:27
         .   .  *.                                                        451264.532668 secs
         .   .   .  *D0                                                   451264.532753 secs D0 => migration/3:33
         .   .   .  *.                                                    451264.532778 secs
         .   .   .   .  *E0                                               451264.532861 secs E0 => migration/4:39
         .   .   .   .  *.                                                451264.532886 secs
         .   .   .   .   .  *F0                                           451264.532973 secs F0 => migration/5:45
      <SNIP>
         A7  A7  A7  A7  A7 *A7  .   .   .   .   .   .   .   .   .   .    451264.790785 secs
         A7  A7  A7  A7  A7  A7 *A7  .   .   .   .   .   .   .   .   .    451264.790858 secs
         A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .   .   .   .   .   .    451264.790934 secs
         A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .   .   .   .   .    451264.791004 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .   .   .   .    451264.791075 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .   .   .    451264.791143 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .   .    451264.791232 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .   .    451264.791336 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .   .    451264.791407 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7  .    451264.791484 secs
         A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7  A7 *A7   451264.791553 secs
      # echo $?
      0
    
    Signed-off-by: Yang Jihong <yangjihong1@huawei.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Link: https://lore.kernel.org/r/20240206083228.172607-4-yangjihong1@huawei.com
    Stable-dep-of: 1a5efc9e13f3 ("libsubcmd: Don't free the usage string")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Wen Zhiwei <wenzhiwei@kylinos.cn>

--- tools/perf/builtin-sched.c
+++ tools/perf/builtin-sched.c
@@ -3306,8 +3306,12 @@ static int perf_sched__map(struct perf_sched *sched)
 {
 	int rc = -1;
 
-	if (setup_map_cpus(sched))
+	sched->curr_thread = calloc(MAX_CPUS, sizeof(*(sched->curr_thread)));
+	if (!sched->curr_thread)
 		return rc;
+
+	if (setup_map_cpus(sched))
+		goto out_free_curr_thread;
 
 	if (setup_color_pids(sched))
 		goto out_put_map_cpus;
@@ -3331,6 +3335,9 @@ out_put_color_pids:
 out_put_map_cpus:
 	zfree(&sched->map.comp_cpus);
 	perf_cpu_map__put(sched->map.cpus);
+
+out_free_curr_thread:
+	zfree(&sched->curr_thread);
 	return rc;
 }
 
@@ -3616,11 +3623,6 @@ int cmd_sched(int argc, const char **argv)
 	unsigned int i;
 	int ret = 0;
 
-	sched.curr_thread = calloc(MAX_CPUS, sizeof(*sched.curr_thread));
-	if (!sched.curr_thread) {
-		ret = -ENOMEM;
-		goto out;
-	}
 	sched.cpu_last_switched = calloc(MAX_CPUS, sizeof(*sched.cpu_last_switched));
 	if (!sched.cpu_last_switched) {
 		ret = -ENOMEM;
@@ -3702,7 +3704,6 @@ int cmd_sched(int argc, const char **argv)
 out:
 	free(sched.curr_pid);
 	free(sched.cpu_last_switched);
-	free(sched.curr_thread);
 
 	return ret;
 }
