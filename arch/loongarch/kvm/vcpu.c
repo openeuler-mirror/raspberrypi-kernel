7a6231f2e5ad8ae989b66c910e14d55b768b919f
Bibo Mao <maobibo@loongson.cn>
2024-09-11 23:26:32 +0800

    LoongArch: KVM: Add vm migration support for LBT registers
    mainline inclusion
    from mainline-v6.12-rc1
    commit acc7f20d54a3eeceec7602b11d6e3462e7fba862
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IAZJDO
    CVE: NA
    
    --------------------------------
    
    Every vcpu has separate LBT registers. And there are four scr registers,
    one flags and ftop register for LBT extension. When VM migrates, VMM
    needs to get LBT registers for every vcpu.
    
    Here macro KVM_REG_LOONGARCH_LBT is added for new vcpu lbt register type,
    the following macro is added to get/put LBT registers.
      KVM_REG_LOONGARCH_LBT_SCR0
      KVM_REG_LOONGARCH_LBT_SCR1
      KVM_REG_LOONGARCH_LBT_SCR2
      KVM_REG_LOONGARCH_LBT_SCR3
      KVM_REG_LOONGARCH_LBT_EFLAGS
      KVM_REG_LOONGARCH_LBT_FTOP
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- arch/loongarch/kvm/vcpu.c
+++ arch/loongarch/kvm/vcpu.c
@@ -803,6 +803,34 @@ static int kvm_get_one_reg(struct kvm_vcpu *vcpu,
 		else
 			ret = -EINVAL;
 		break;
+	case KVM_REG_LOONGARCH_LBT:
+		if (!kvm_guest_has_lbt(&vcpu->arch))
+			return -ENXIO;
+
+		switch (reg->id) {
+		case KVM_REG_LOONGARCH_LBT_SCR0:
+			*v = vcpu->arch.lbt.scr0;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR1:
+			*v = vcpu->arch.lbt.scr1;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR2:
+			*v = vcpu->arch.lbt.scr2;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR3:
+			*v = vcpu->arch.lbt.scr3;
+			break;
+		case KVM_REG_LOONGARCH_LBT_EFLAGS:
+			*v = vcpu->arch.lbt.eflags;
+			break;
+		case KVM_REG_LOONGARCH_LBT_FTOP:
+			*v = vcpu->arch.fpu.ftop;
+			break;
+		default:
+			ret = -EINVAL;
+			break;
+		}
+		break;
 	case KVM_REG_LOONGARCH_KVM:
 		switch (reg->id) {
 		case KVM_REG_LOONGARCH_COUNTER:
@@ -864,6 +892,34 @@ static int kvm_set_one_reg(struct kvm_vcpu *vcpu,
 		if (id == LOONGARCH_CPUCFG6) {
 			vcpu->arch.max_pmu_csrid = LOONGARCH_CSR_PERFCTRL0 +
 							2 * kvm_get_pmu_num(&vcpu->arch) + 1;
+		}
+		break;
+	case KVM_REG_LOONGARCH_LBT:
+		if (!kvm_guest_has_lbt(&vcpu->arch))
+			return -ENXIO;
+
+		switch (reg->id) {
+		case KVM_REG_LOONGARCH_LBT_SCR0:
+			vcpu->arch.lbt.scr0 = v;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR1:
+			vcpu->arch.lbt.scr1 = v;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR2:
+			vcpu->arch.lbt.scr2 = v;
+			break;
+		case KVM_REG_LOONGARCH_LBT_SCR3:
+			vcpu->arch.lbt.scr3 = v;
+			break;
+		case KVM_REG_LOONGARCH_LBT_EFLAGS:
+			vcpu->arch.lbt.eflags = v;
+			break;
+		case KVM_REG_LOONGARCH_LBT_FTOP:
+			vcpu->arch.fpu.ftop = v;
+			break;
+		default:
+			ret = -EINVAL;
+			break;
 		}
 		break;
 	case KVM_REG_LOONGARCH_KVM:
