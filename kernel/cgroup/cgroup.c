29acdd8404e35e99b3598601c87914ab4c7b9b6e
Phil Elwell <phil@raspberrypi.com>
2024-12-09 14:58:16 +0000

    fixup! cgroup: Use kernel command line to disable memory cgroup
    cgroup features are distinct from cgroup subsystems - handle them
    correctly.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -6979,10 +6979,18 @@ static int __init cgroup_enable(char *str)
 			    strcmp(token, ss->legacy_name))
 				continue;
 
-			cgroup_feature_disable_mask &= ~(1 << i);
 			static_branch_enable(cgroup_subsys_enabled_key[i]);
 			pr_info("Enabling %s control group subsystem\n",
 				ss->name);
+		}
+
+		for (i = 0; i < OPT_FEATURE_COUNT; i++) {
+			if (strcmp(token, cgroup_opt_feature_names[i]))
+				continue;
+			cgroup_feature_disable_mask &= ~(1 << i);
+			pr_info("Enabling %s control group feature\n",
+				cgroup_opt_feature_names[i]);
+			break;
 		}
 	}
 	return 1;
