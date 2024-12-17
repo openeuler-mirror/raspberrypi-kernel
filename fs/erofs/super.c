78c054ed0bd7bf57c2d4f39a0d12f1099583f951
Zizhi Wo <wozizhi@huawei.com>
2024-12-17 20:21:54 +0800

    erofs: add erofs_ondemand switch
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB5UKT
    
    --------------------------------
    
    In order to make the user state better control the characteristics of the
    on demand load mode, a dynamic switch about the on-demand mode is added.
    
    The switch named cachefiles_ondemand_enabled is closed by default, and is
    isolated by CONFIG_CACHEFILES_ONDEMAND. Users can open it by echo
    1/on/ON/y/Y to /sys/module/fs_ctl/parameters/cachefiles_ondemand_enabled.
    
    Signed-off-by: Zizhi Wo <wozizhi@huawei.com>

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -514,12 +514,20 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 		break;
 #ifdef CONFIG_EROFS_FS_ONDEMAND
 	case Opt_fsid:
+		if (!sbi->ondemand_enabled) {
+			errorfc(fc, "fsid option not supported");
+			break;
+		}
 		kfree(sbi->fsid);
 		sbi->fsid = kstrdup(param->string, GFP_KERNEL);
 		if (!sbi->fsid)
 			return -ENOMEM;
 		break;
 	case Opt_domain_id:
+		if (!sbi->ondemand_enabled) {
+			errorfc(fc, "domain_id option not supported");
+			break;
+		}
 		kfree(sbi->domain_id);
 		sbi->domain_id = kstrdup(param->string, GFP_KERNEL);
 		if (!sbi->domain_id)
@@ -762,11 +770,16 @@ static const struct fs_context_operations erofs_context_ops = {
 	.free		= erofs_fc_free,
 };
 
+static inline bool erofs_can_init(void)
+{
+	return READ_ONCE(erofs_enabled) || cachefiles_ondemand_is_enabled();
+}
+
 static int erofs_init_fs_context(struct fs_context *fc)
 {
 	struct erofs_sb_info *sbi;
 
-	if (!READ_ONCE(erofs_enabled))
+	if (!erofs_can_init())
 		return -EOPNOTSUPP;
 
 	sbi = kzalloc(sizeof(*sbi), GFP_KERNEL);
@@ -778,6 +791,7 @@ static int erofs_init_fs_context(struct fs_context *fc)
 		kfree(sbi);
 		return -ENOMEM;
 	}
+	sbi->ondemand_enabled = cachefiles_ondemand_is_enabled();
 	fc->s_fs_info = sbi;
 
 	idr_init(&sbi->devs->tree);
