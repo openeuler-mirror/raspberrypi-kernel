c61525d00102f7922a0de825124db4446a57d1c3
Jonathan Bell <jonathan@raspberrypi.com>
2025-01-08 15:09:53 +0000

    mmc: sd: filter card CQ support based on an allow-list
    We have found that many SD cards in the field, even of the same make and
    model, have latent bugs in their CQ implementation. Some product lines
    have fewer bugs with newer manufacture dates, but this is not a
    guarantee that a particular card is at a particular firmware revision
    level.
    
    Many of these bugs lead to card hangs or data corruption. Add a quirk to
    mark a card as having a tested, working CQ implementation and ignore the
    capability if absent.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/mmc/core/card.h
+++ drivers/mmc/core/card.h
@@ -285,4 +285,10 @@ static inline int mmc_card_broken_cache_flush(const struct mmc_card *c)
 {
 	return c->quirks & MMC_QUIRK_BROKEN_CACHE_FLUSH;
 }
+
+static inline int mmc_card_working_sd_cq(const struct mmc_card *c)
+{
+	return c->quirks & MMC_QUIRK_WORKING_SD_CQ;
+}
+
 #endif
